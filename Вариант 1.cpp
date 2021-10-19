38032e585d288f6fba57efbf68578c2454d23554
F1lya17 <90614029+F1lya17@users.noreply.github.com>
2021-10-19 11:34:19 +0500

    Create Вариант 1.cpp
    Cдал задачу 2_1

---
+++ Вариант 1.cpp
@@ -0,0 +1,40 @@
+/* Даны два массива целых чисел одинаковой длины A[0..n-1] и B[0..n-1]. Необходимо найти первую пару индексов i0 и j0, , такую что A[i0] + B[j0] = max {A[i] + B[j], где 0 <= i < n, 0 <= j < n, i <= j}.
+
+Время работы - O(n). n ≤ 100000.
+54935432
+https://contest.yandex.ru/contest/29212/problems/2_1/
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, iA = 0, iB = 0;
+    cin >> n;
+    int *A = new int[n];
+    int *B = new int[n];
+    for (int i = 0; i < n; i++){
+        cin >> A[i];
+    }
+    for (int i = 0; i < n; i++){
+        cin >> B[i];
+    }
+    int mx = A[0];
+    int Sum = A[0] + B[0];
+    int max_index = 0;
+    for (int i = 0; i < n; i++){
+        if (A[i] > mx){
+            mx = A[i];
+            max_index = i;
+        }
+        if (mx + B[i] > Sum){
+            iA = max_index;
+            iB = i;
+            Sum = mx + B[i];
+        }
+    }
+    cout << iA << " " << iB;
+    delete[] A;
+    delete[] B;
+}
